7207be8371c848ea363ec1f99708168010fe00e8
deepak-singh-123 <deepak19970503@gmail.com>
2024-05-23 01:56:38 +0530

    csv formate done in dashboard page on change

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,40 +1,57 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head, useForm } from '@inertiajs/react';
 import { useState } from 'react';
 
-import { Button, Alert, Tabs, Tab } from 'react-bootstrap';
+import { Button, Alert, Tabs, Tab, Table } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Dashboard({ auth }) {
 
+
+
+
+
+    const { data, setData, errors, post, progress } = useForm({
+        name: "Deepak",
+        csv_data: [
+            {"Column-1": "India", "Column-2": "Bihar", "Column-3": "Motihari", "": "sec-63"},
+            {"Column-1": "India", "Column-2": "UP", "Column-3": "Noida", "Column-4": "sec-155, UP"},
+            {"Column-1": "India", "Column-2": "Punjab", "Column-3": "Mohali", "Column-4": "sec-110, punjab"},
+        ]
+    });
     
     const options = [
-        { value: 'apple', label: 'Apple' },
-        { value: 'banana', label: 'Banana' },
-        { value: 'cherry', label: 'Cherry' },
-        { value: 'date', label: 'Date' },
-        { value: 'name', label: 'name' },
-        { value: 'address', label: 'Address' },
+        { value: 'name', label: 'Name' },
+        { value: 'state', label: 'State' },
         { value: 'city', label: 'City' },
+        { value: 'date', label: 'Date' },
+        { value: 'address', label: 'Address' },
+        { value: 'country', label: 'Country' },
+        { value: 'Column-1', label: 'Column-1' },
     ];
-    
-    // How many select boxes
-    const selectBoxes = ['Select-1', 'Select-2', 'Select-3'];
+    // Column List
+    const [columnList, setColumnList] = useState(['Column-1', "Column-2", "Column-3", "Column-4"]);
 
-    // Create state for each select box
-    const [selectedValues, setSelectedValues] = useState(['name']);
-
-    // Handle change for select boxes
-    const handleChange = (index, event) => {
-        const newSelectedValues = [...selectedValues];
-        newSelectedValues[index] = event.target.value;
-        setSelectedValues(newSelectedValues);
+    const changeKeyName = (oldKey, newKey) => {
+        const updatedCsvData = data.csv_data.map(item => {
+            const newItem = { ...item };
+            if (newItem.hasOwnProperty(oldKey)) {
+                newItem[newKey] = newItem[oldKey];
+                delete newItem[oldKey];
+            }
+            return newItem;
+        });
+        setData('csv_data', updatedCsvData);
     };
-    console.warn(selectedValues);
 
-
-
-
+    const handleChange = (oldKey, index, event) => {
+        var newKey = event.target.value;
+        const newSelectedValues = [...columnList];
+        newSelectedValues[index] = newKey;
+        setColumnList(newSelectedValues);
+        // console.warn("Old key = "+oldKey+ ", New key = "+newKey);
+        changeKeyName(oldKey, newKey);
+    }
 
     return (
         <AuthenticatedLayout
@@ -53,49 +70,51 @@ export default function Dashboard({ auth }) {
 
 
 
-                            <div>
-                                {
-                                    selectBoxes.map((defaultValue, index) => (
-                                        
-                                        <select
-                                            key={index}
-                                            value={selectedValues[index]}
-                                            onChange={(event) => handleChange(index, event)} >
-                                            {
-                                                options.map((option) => (
-                                                    <option key={option.value} value={option.value}>
-                                                        {option.label}
-                                                    </option>
-                                                ))
-                                            }
-                                        </select>
-                                        
-                                    ))
-                                }
-                            </div>
+                            <Table stripped bordered hover size="sm" >
+                                <thead>
+                                    <tr>
+                                        <th>Column</th>
+                                        <th>Column name</th>
+                                        <th>Column map to</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        columnList.map((defaultValue, index) => (
+                                            <tr>
+                                                <td>{index}</td>
+                                                <td>{defaultValue}</td>
+                                                <td>
+                                                    <select
+                                                        key={index}
+                                                        value={defaultValue}
+                                                        onChange={(event) => handleChange(defaultValue, index, event)}
+                                                    >
+                                                        {
+                                                            options.map((option) => (
+                                                                <option key={option.value} value={option.value}>
+                                                                    {option.label}
+                                                                </option>
+                                                            ))
+                                                        }
+                                                    </select>
+
+                                                </td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+
+                            </Table>
+                            
+                            <h3>Change Data</h3>
+                            <pre>{JSON.stringify(data.csv_data, null, 2)}</pre>
 
 
 
 
 
 
-
-                            {/* <Tabs
-                                defaultActiveKey="home"
-                                id="justify-tab-example"
-                                justify >
-                                <Tab eventKey="home" title="Home">
-                                    <div className='px-3 py-3' style={{'borderRight':'1px solid silver', 'borderBottom':'1px solid silver', 'borderLeft':'1px solid silver'}} >
-                                        Tab content for Home
-                                    </div>
-                                </Tab>
-                                <Tab eventKey="profile" title="Profile">
-                                    Tab content for Profile
-                                </Tab>
-                                <Tab eventKey="longer-tab" title="Loooonger Tab">
-                                    Tab content for Loooonger Tab
-                                </Tab>
-                            </Tabs> */}
 
                         </div>
                     </div>
